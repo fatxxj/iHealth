626f85e36948d1ff6141fb28ee76db5a2f766216
Fat Halimi <fat.halimi@i-solve.info>
2022-09-17 17:06:46 +0200

    Commit: Issue-4 #4

---
+++ iHealthAPI/Data/AppDbContext.cs
@@ -0,0 +1,71 @@
+﻿using iHealthAPI.Models;
+using iHealthAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+
+namespace iHealthAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private const string appUser = "SampleApplication";
+
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
+        {
+            Database.EnsureCreated();
+            if (httpContextAccessor != null)
+            {
+                _httpContextAccessor = httpContextAccessor;
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            AddAuditInfo();
+            return base.SaveChanges();
+        }
+
+
+        private void AddAuditInfo()
+        {
+            var entities = ChangeTracker.Entries<IEntity>().Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            var utcNow = DateTime.UtcNow;
+            var user = _httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? appUser;
+            var ipAddress = _httpContextAccessor?.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+
+            foreach (var entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    entity.Entity.CreatedOnUtc = utcNow;
+                    entity.Entity.CreatedBy = user;
+                }
+
+                if (entity.State == EntityState.Modified)
+                {
+                    entity.Entity.LastModifiedOnUtc = utcNow;
+                    entity.Entity.LastModifiedBy = user;
+                }
+
+                entity.Entity.IPAddress = ipAddress;
+            }
+        }
+
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
