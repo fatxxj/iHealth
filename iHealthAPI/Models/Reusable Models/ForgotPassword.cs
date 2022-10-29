496dc9dc267cf212b3221b69098fb3309f3e32f8
Fat Halimi <halimifat@gmail.com>
2022-10-29 14:29:49 +0200

    Commit: Issue-38 #38

---
+++ iHealthAPI/Models/Reusable Models/ForgotPassword.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iHealthAPI.Models
+{
+    public class ForgotPassword
+    {
+        public string Email { get; set; }
+    }
+}
